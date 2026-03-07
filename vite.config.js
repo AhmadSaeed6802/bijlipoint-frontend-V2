e7f5459ea4b454f4c7caf5968a1752e17e4ec60a
Ahmad Saeed <139094382+AhmadSaeed6802@users.noreply.github.com>
2026-03-08 03:53:01 +0500

    update(base): base path

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,5 @@
-import{defineConfig}from'vite';import react from'@vitejs/plugin-react';export default defineConfig({plugins:[react()],base:'/',server:{port:3000},build:{outDir:'dist'}});
+import{defineConfig}from'vite';import react from'@vitejs/plugin-react';
+export default defineConfig({
+    plugins:[react()],
+    base: '/bijlipoint-frontend-V2/',
+    server:{port:3000},build:{outDir:'dist'}});
