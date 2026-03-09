db2521fb94e08801545dd8a2168338cd66d1e4e0
Ahmad Saeed <139094382+AhmadSaeed6802@users.noreply.github.com>
2026-03-09 22:29:50 +0500

    update(api): updated for server ip based api end points

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,7 @@
 const API_URL =
   process.env.NODE_ENV === "development"
-    ? "https://localhost:49597"   // backend during local dev
-    : "https://bijlipoint.com";   // backend in production
+    ? "https://localhost:49597/api"   // backend during local dev
+    : "http://156.67.29.207:5000/api";
+    //: "https://bijlipoint.com";   // backend in production
 
 export default API_URL;
