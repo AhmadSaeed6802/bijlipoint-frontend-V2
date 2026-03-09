f77de04f1dfbdb485ad0cd971d32ad354eedae2c
Ahmad Saeed <139094382+AhmadSaeed6802@users.noreply.github.com>
2026-03-10 00:40:03 +0500

    update(api): api bijlipoint com dmain name assing instead of IP

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 const API_URL =
   process.env.NODE_ENV === "development"
     ? "https://localhost:49597/api"   // backend during local dev
-    : "http://156.67.29.207:5000/api";
+    : "https://api.bijlipoint.com/api";
     //: "https://bijlipoint.com";   // backend in production
 
 export default API_URL;
